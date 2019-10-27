5334641719965a2a6e443963287931f49664445c
kenta-s <knt01222@gmail.com>
2019-10-27 23:18:17 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   # skip_before_action :verify_authenticity_token, if: :devise_controller?
 
-  # http://127.0.0.1:3000/api/v1/auth/twitter?omniauth_window_type=newWindow
+  # http://127.0.0.1:3000/api/v1/auth/twitter?auth_origin_url=https://www.example.com
   def twitter_client
     @twitter_client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
